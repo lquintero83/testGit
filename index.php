fa238c205b74e728365d0f492099d676d237b019
lquintero <lquintero@01-1820.psa.local>
2016-09-08 10:28:05 -0300

    segundo commit

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
+        
+        
+        /* se modifica*/
+        
         ?>
     </body>
 </html>
